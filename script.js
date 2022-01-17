b02714b890782e036288d394867c022cd526eca9
Abhishekkardamm <abhishekkardamm@gmail.com>
2022-01-17 00:27:30 +0000

    some sort of commit message

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+function updateRate() {
+    var first = parseInt(document.getElementById("principal").value);
+    var second = parseInt(document.getElementById("rate").value);
+    var third = parseInt(document.getElementById("years").value);
+  
+    var final = (first * second * third) / 100;
+  
+    var years = new Date().getFullYear() + parseInt(third);
+  
+    var Full_info =
+      "If you deposit " +
+      first +
+      "<br/>" +
+      "at an intrest rate of  " +
+      second +
+      " %" +
+      "<br/>" +
+      " You will receive an amount of " +
+      final +
+      "," +
+      "<br/>" +
+      " in the year " +
+      years;
+  
+    document.getElementById("result").innerHTML = Full_info;
+  }
+  function updateRate() {
+    var first = parseInt(document.getElementById("principal").value);
+    var second = parseInt(document.getElementById("rate").value);
+    var third = parseInt(document.getElementById("years").value);
+  
+    var final = (first * second * third) / 100;
+  
+    var years = new Date().getFullYear() + parseInt(third);
+  
+    var Full_info =
+      "If you deposit " +
+      first +
+      "<br/>" +
+      "at an intrest rate of  " +
+      second +
+      " %" +
+      "<br/>" +
+      " You will receive an amount of " +
+      final +
+      "," +
+      "<br/>" +
+      " in the year " +
+      years;
+  
+    document.getElementById("result").innerHTML = Full_info;
+  }
+  
